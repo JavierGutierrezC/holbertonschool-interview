76925b4e2465ff1efafa7a9390ee4962d06bd569
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-04-18 18:33:36 +0000

    [Fix]pep8 return

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,3 +1,24 @@
 #!/usr/bin/python3
 
-def canUnlockAll(boxes)
+
+def canUnlockAll(boxes):
+
+    key = 0
+    KeyC = [0]
+    for llave in KeyC:
+        for key in boxes[llave]:
+            # print("estos es boxes key",boxes[llave])
+            # print("estos es keys",key)
+            if key in KeyC:
+                # llavero.append(key)
+                # print(KeyC)
+                # continue
+            else:
+                KeyC.append(key)
+                # key = keys
+                # a = keys
+                # break
+    if len(KeyC) == len(boxes):
+        return(True)
+    else:
+        return(False)
