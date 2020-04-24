9fbdc1eade88d19ea9946a1059ac881ac1f758cc
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-04-24 21:33:11 +0000

    [Fix]conditions

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * insert_node - insert new node
+ * @head: first element of the list
+ * @number: number to be added
+ * Return: New node
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+listint_t *act, *new;
+
+act = *head;
+new = malloc(sizeof(listint_t));
+
+if (new == NULL)
+return (NULL);
+new->n = number;
+
+if (act == NULL || (act)->n >= new->n)
+{
+new->next = act;
+*head = new;
+return (new);
+}
+
+act = *head;
+
+while (act->next->n < new->n)
+{
+act = act->next;
+}
+new->next = act->next;
+act->next = new;
+return (new);
+}
