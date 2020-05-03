5d79ec1104552fe54217e11d96b15d0d0f55e1cb
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-05-03 19:34:25 +0000

    [Fix]betty comments

--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -9,7 +9,7 @@
 #include <limits.h>
 
 /**
- * struct binary_tree_T - Binary tree node
+ * struct binary_tree_s - Binary tree node
  *
  * @n: Integer stored in the node
  * @parent: Pointer to the parent node
@@ -19,10 +19,10 @@
 
 typedef struct binary_tree_s
 {
-  int n;
-  struct binary_tree_s *parent;
-  struct binary_tree_s *left;
-  struct binary_tree_s *right;
+int n;
+struct binary_tree_s *parent;
+struct binary_tree_s *left;
+struct binary_tree_s *right;
 } binary_tree_t;
 
 typedef struct binary_tree_s heap_t;
