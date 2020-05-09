757d40af34a0ae4e9dc27a0f52684f022d812ee0
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-05-09 23:32:51 +0000

    [Fix]All

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -5,31 +5,50 @@
 def minOperations(n):
     """a method that calculates the fewest number of operations
     needed to result in exactly n H characters in the file.'"""
-    i = 1
-    p = n
-    co = 0
-    pa = 0
-    if (n < 1):
-        return(0)
-    else:
-        while i < n:
-            # print("esto es i", i)
-            if (n % 2 == 0):
-                n = n / 2
-                # print("im n if",n)
-                co = co + 1
-                pa = pa + 1
+    if type(n) is not int:
+        return 0
+    if n < 1:
+        return 0
+    x = 1
+    p = 0
+    cop = pas = 0
+    copy = 0
+    paste = 1
+    ant = 1
+    nhp1 = nhp2 = nhp3 = 0
+    while x < n:
+        if n % (x * 2) == 0:
+            if nhp1 == 1:
+                nhp1 = 0
+            nhp3 = 0
+            cop += 1
+            ant = x
+            pas += 1
+            x = 2 * x
+            copy += ant
+            paste += (2 * ant) - paste
+        elif n % x == 0 and x != 1:
+            if n % paste == 0:
+                ant = paste
+                nhp1 = 0
+                x = paste
             else:
-                n = n + 1
-                # print("im n else",n)
-                n = n / 2
-                # print("im n else div",n)
-                pa = pa + 1
-        a = 0
-        for i in range(2, p - 1):
-            if (p % i) == 0:
-                print(p)
-                a = 1
-        if a == 0:
-            return(p)
-        return(co + pa if p % 2 == 0 else co + pa + 1)
+                ant = x
+            nhp3 = 0
+            if nhp1 == 0:
+                nhp1 = 1
+                cop += 1
+            pas += 1
+            paste += ant
+        else:
+            nhp1 = 0
+            if nhp3 == 0:
+                nhp3 = 1
+                cop += 1
+                ant = x
+            x = x + ant
+            pas += 1
+            paste += ant
+        if paste >= n:
+            break
+    return cop + pas
