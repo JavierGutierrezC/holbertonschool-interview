ffa9869838956b77f61204c8dd3dabe7ce4f4726
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-05-09 20:26:23 +0000

    [Fix]condition for prime

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -9,21 +9,27 @@ def minOperations(n):
     p = n
     co = 0
     pa = 0
-    for i in range(1, n+1):
-        if (n % i) != 0:
-            return(n)
-        else:
-            while i < n:
-                # print("esto es i", i)
-                if (n % 2 == 0):
-                    n = n / 2
-                    # print("im n if",n)
-                    co = co + 1
-                    pa = pa + 1
-                else:
-                    n = n + 1
-                    # print("im n else",n)
-                    n = n / 2
-                    # print("im n else div",n)
-                    pa = pa + 1
-            return(co + pa if p % 2 == 0 else co + pa + 1)
+    if (n < 1):
+        return(0)
+    else:
+        while i < n:
+            # print("esto es i", i)
+            if (n % 2 == 0):
+                n = n / 2
+                # print("im n if",n)
+                co = co + 1
+                pa = pa + 1
+            else:
+                n = n + 1
+                # print("im n else",n)
+                n = n / 2
+                # print("im n else div",n)
+                pa = pa + 1
+        a = 0
+        for i in range(2, p - 1):
+            if (p % i) == 0:
+                print(p)
+                a = 1
+        if a == 0:
+            return(p)
+        return(co + pa if p % 2 == 0 else co + pa + 1)
