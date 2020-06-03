ccc95ca8ff4f1fdb6aabeb0424a050728457f39d
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-06-03 16:03:28 +0000

    [Fix]Order

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -2,34 +2,34 @@
 """ script that reads stdin line by line and computes metrics"""
 
 import sys
-import fileinput
 
-increase = 1
-status_code = [0, 0, 0, 0, 0, 0, 0, 0]
-size = 0
-status_L = ["200", "301", "400", "401", "403", "404", "405", "500"]
+if __name__ == "__main__":
 
+    increase = 1
+    status_code = [0, 0, 0, 0, 0, 0, 0, 0]
+    size = 0
+    status_L = ["200", "301", "400", "401", "403", "404", "405", "500"]
 
-def signal_print(status_code, status_L):
-    """ Method to handle ctrl-c"""
-    print("File size: {}".format(size))
-    for i in range(len(status_code)):
-        if status_code[i] > 0:
-            print("{}: {}".format(status_L[i], status_code[i]))
+    def signal_print(status_code, status_L):
+        """ Method to handle ctrl-c"""
+        print("File size: {}".format(size))
+        for i in range(len(status_code)):
+            if status_code[i] > 0:
+                print("{}: {}".format(status_L[i], status_code[i]))
 
-try:
-    for line in sys.stdin:
-        each_argvs = line.split(" ")
-        if len(each_argvs) == 9:
-            status = each_argvs[-2]
-            if status in status_L:
-                i = status_L.index(status)
-                status_code[i] = status_code[i] + 1
-                size = size + int(each_argvs[-1][:-1])
+    try:
+        for line in sys.stdin:
+            each_argvs = line.split(" ")
+            if len(each_argvs) > 6:
+                status = each_argvs[-2]
+                if status in status_L:
+                    i = status_L.index(status)
+                    status_code[i] = status_code[i] + 1
+                size = size + int(each_argvs[-1][:])
                 if increase % 10 == 0:
                     signal_print(status_code, status_L)
-        increase = increase + 1
-except KeyboardInterrupt:
+            increase = increase + 1
+    except KeyboardInterrupt:
+        signal_print(status_code, status_L)
+        raise
     signal_print(status_code, status_L)
-    raise
-signal_print(status_code, status_L)
