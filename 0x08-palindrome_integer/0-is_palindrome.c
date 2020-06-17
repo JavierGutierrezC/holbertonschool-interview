a35fffb59baaa220c6d7ffcfa5ecff55fc368973
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-06-17 17:24:55 +0000

    [Fix]Temove one cycle

--- 0x08-palindrome_integer/0-is_palindrome.c
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -9,17 +9,13 @@
 int is_palindrome(unsigned long n)
 {
   int unit = 0;
-  unsigned long int diez = 1;
-  unsigned long int diez_up = 1;
   unsigned long int rev_n = 0;
   unsigned long int orig = n;
-  while (diez >= 1)
+  while (n >= 1)
     {
-      unit = n / diez;
-      n = n - (unit * diez);
-      rev_n = rev_n + (unit * diez_up);
-      diez_up = diez_up * 10;
-      diez = diez / 10;
+      unit = n % 10;
+      rev_n = (rev_n *(10)) + unit;
+      n = n / 10;
     }
   return (orig == rev_n);
 }
