79d3806de25798c2ddce7216f65445f57ca3ff08
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-08-19 01:41:25 +0000

    [Fix]style

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -5,9 +5,6 @@ function requesito (url) {
   return new Promise((resolve, reject) => {
     request(url, (error, response, body) => {
       if (error) reject(error);
-      /* if (response.statusCode !== 200) {
-        reject('Invalid status code <' + response.statusCode + '>');
-      } */
       resolve(body);
     });
   });
@@ -18,10 +15,10 @@ request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], async function
   if (error !== null) {
     console.error(error);
   }
-  let a = 0;
-  while (a < characters.length) {
+  let x = 0;
+  while (x < characters.length) {
     try {
-      const body = await requesito(characters[a]);
+      const body = await requesito(characters[x]);
       console.log(JSON.parse(body).name);
     } catch (error) {
       console.error(error);
