f53116e47c290414a7371576cda5f2a4e6207bfc
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-08-19 01:30:01 +0000

    Fixed error

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+const request = require('request');
+
+function requesito (url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) reject(error);
+      if (response.statusCode !== 200) {
+        reject('Invalid status code <' + response.statusCode + '>');
+      }
+      resolve(body);
+    });
+  });
+}
+
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], async functio\
+n (error, response, body) {
+  const characters = JSON.parse(body).characters;
+  if (error !== null) {
+    console.error(error);
+  }
+  let a = 0;
+  while (a < characters.length) {
+    try {
+          const body = await requesito(characters[a]);
+          console.log(JSON.parse(body).name);
+    } catch (error) {
+          console.error(error);
+    }
+
+    a = a + 1;
+  }
+});
