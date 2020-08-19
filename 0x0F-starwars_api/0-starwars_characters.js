d38d2a086181dec32312d9945bb9d3bd8a979b8d
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-08-19 01:36:32 +0000

    [Fix]Executable

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -5,16 +5,15 @@ function requesito (url) {
   return new Promise((resolve, reject) => {
     request(url, (error, response, body) => {
       if (error) reject(error);
-      if (response.statusCode !== 200) {
+      /* if (response.statusCode !== 200) {
         reject('Invalid status code <' + response.statusCode + '>');
-      }
+      } */
       resolve(body);
     });
   });
 }
 
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], async functio\
-n (error, response, body) {
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], async function (error, response, body) {
   const characters = JSON.parse(body).characters;
   if (error !== null) {
     console.error(error);
@@ -22,12 +21,13 @@ n (error, response, body) {
   let a = 0;
   while (a < characters.length) {
     try {
-          const body = await requesito(characters[a]);
-          console.log(JSON.parse(body).name);
+      const body = await requesito(characters[a]);
+      console.log(JSON.parse(body).name);
     } catch (error) {
-          console.error(error);
+      console.error(error);
     }
 
     a = a + 1;
   }
 });
+
